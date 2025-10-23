317d7f11912bddfc39c756c45e15eadba87172bb
guest75582332 <221468344+guest75582332@users.noreply.github.com>
2025-10-23 16:41:07 +0800

    add chaotic-aur repo
    Signed-off-by: guest75582332 <221468344+guest75582332@users.noreply.github.com>

--- build-aur-action/entrypoint.sh
+++ build-aur-action/entrypoint.sh
@@ -9,9 +9,15 @@ chmod -R a+rw .
 cat << EOM >> /etc/pacman.conf
 [archlinuxcn]
 Server = https://repo.archlinuxcn.org/x86_64
+[chaotic-aur]
+Include = /etc/pacman.d/chaotic-mirrorlist
 EOM
 
 pacman-key --init
+pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+pacman-key --lsign-key 3056513887B78AEB
+pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
+pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 pacman-key --lsign-key "farseerfc@archlinux.org"
 pacman -Sy --noconfirm && pacman -S --noconfirm archlinuxcn-keyring
 pacman -Syu --noconfirm archlinux-keyring
@@ -21,6 +27,5 @@ if [ ! -z "$INPUT_PREINSTALLPKGS" ]; then
 fi
 
 sudo --set-home -u builder yay -S --noconfirm --builddir=./ "$pkgname"
-ls -al
 cd "./$pkgname" || exit 1
 python3 ../build-aur-action/encode_name.py
