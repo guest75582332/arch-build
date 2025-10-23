6e6e284deac0f82adbe1e76daf0d85e7e68e7aa5
guest75582332 <221468344+guest75582332@users.noreply.github.com>
2025-10-23 16:47:59 +0800

    fix CI error
    Signed-off-by: guest75582332 <221468344+guest75582332@users.noreply.github.com>

--- build-aur-action/entrypoint.sh
+++ build-aur-action/entrypoint.sh
@@ -6,6 +6,8 @@ useradd builder -m
 echo "builder ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 chmod -R a+rw .
 
+pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+pacman-key --lsign-key 3056513887B78AEB
 pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
 pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 
@@ -17,8 +19,6 @@ Include = /etc/pacman.d/chaotic-mirrorlist
 EOM
 
 pacman-key --init
-pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
-pacman-key --lsign-key 3056513887B78AEB
 pacman-key --lsign-key "farseerfc@archlinux.org"
 pacman -Sy --noconfirm && pacman -S --noconfirm archlinuxcn-keyring
 pacman -Syu --noconfirm archlinux-keyring
