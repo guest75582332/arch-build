7203c3ffc04b15c609839ce34488be9394ae5812
guest75582332 <221468344+guest75582332@users.noreply.github.com>
2026-01-26 12:21:41 +0800

    Update entrypoint.sh
    Signed-off-by: guest75582332 <221468344+guest75582332@users.noreply.github.com>

--- build-aur-action/entrypoint.sh
+++ build-aur-action/entrypoint.sh
@@ -8,13 +8,19 @@ useradd builder -m
 echo "builder ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 chmod -R a+rw .
 
+pacman-key --init
+pacman-key --recv-key 3056513887B78AEB
+pacman-key --lsign-key 3056513887B78AEB
+pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
+pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+
 cat << EOM >> /etc/pacman.conf
 [archlinuxcn]
 Server = https://repo.archlinuxcn.org/x86_64
+[chaotic-aur]
+Include = /etc/pacman.d/chaotic-mirrorlist
 EOM
 
-pacman-key --init
-pacman-key --lsign-key "farseerfc@archlinux.org"
 pacman -Sy --noconfirm && pacman -S --noconfirm archlinuxcn-keyring
 pacman -Syu --noconfirm archlinux-keyring
 pacman -S --noconfirm yay
